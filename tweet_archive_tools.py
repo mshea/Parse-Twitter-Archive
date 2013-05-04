b336dcbbaccc1af86d06b65d19f27980995bb1a9
Mike Shea <mike@mikeshea.net>
2013-05-03 22:39:05 -0400

    PEP8

--- tweet_archive_tools.py
+++ tweet_archive_tools.py
@@ -33,70 +33,70 @@ from itertools import islice, izip
 from collections import Counter
 
 params = {
-	'data_files': './mshea_tweets/data/js/tweets/*.js',
-	'geo_output': 'mshea_geo.csv',
-	'text_output': 'mshea_tweets.txt',
-	'json_output': 'mshea_tweets.json',
-	'bff_output': 'mshea_bffs.csv',
-	'csv_output': 'mshea_tweets.csv',
-	'sqlite3_output': 'mshea_tweets.sqlite3',
-	'html_output': 'mshea_tweets.html',
-	'text_output': 'mshea_tweets.txt',
-	'twitter_user_id': 'mshea',
-	}
+    'data_files': './mshea_tweets/data/js/tweets/*.js',
+    'geo_output': 'mshea_geo.csv',
+    'text_output': 'mshea_tweets.txt',
+    'json_output': 'mshea_tweets.json',
+    'bff_output': 'mshea_bffs.csv',
+    'csv_output': 'mshea_tweets.csv',
+    'sqlite3_output': 'mshea_tweets.sqlite3',
+    'html_output': 'mshea_tweets.html',
+    'text_output': 'mshea_tweets.txt',
+    'twitter_user_id': 'mshea',
+}
 
 
 def load_data(files):
-	items = []
-	files = glob.glob(files)
-	for file in files:
-		with open(file) as f:
-			d = f.readlines()[1:] # get rid of first line
-			d = "".join(d) # turn it into a string
-			j = json.loads(d) # Parse the JSON
-			for tweet in j:
-				items.append(tweet)
-	return items
+    items = []
+    files = glob.glob(files)
+    for file in files:
+        with open(file) as f:
+            d = f.readlines()[1:]  # Twitter's JSON first line is bogus
+            d = "".join(d)
+            j = json.loads(d)
+            for tweet in j:
+                items.append(tweet)
+    return items
 
 
 def get_bffs(d):
-	words = []
-	for item in d:
-		item_words = item['text'].split()
-		for word in item_words:
-			if '@' in word:
-				words.append(word.replace(':', '').lower())
-	return collections.Counter(words).most_common(50)
+    words = []
+    for item in d:
+        item_words = item['text'].split()
+        for word in item_words:
+            if '@' in word:
+                words.append(word.replace(':', '').lower().encode('utf-8'))
+    return collections.Counter(words).most_common(50)
 
 
 def get_bigrams(d):
-	words = []
-	for item in d:
-		item_words = re.findall('\w+', item['text'])
-		words += item_words
-	output = (Counter(zip(words,words[1:])).most_common(100))
-	for item in output:
-		print item
+    words = []
+    for item in d:
+        item_words = re.findall('\w+', item['text'])
+        words += item_words
+    output = (Counter(zip(words, words[1:])).most_common(100))
+    for item in output:
+        print item
 
 
 def get_geo(d):
-	output = [('date', 'tweet', 'lat', 'long')]
-	for item in d:
-		try:
-			lat = item['geo']['coordinates'][0]
-			long = item['geo']['coordinates'][1]
-			date = item['created_at']
-			text = item['text'].encode('utf-8')
-			output.append((date, text, lat, long))
-		except:
-			error = "no coordinates"
-	return output
+    output = [('date', 'tweet', 'lat', 'long')]
+    for item in d:
+        try:
+            lat = item['geo']['coordinates'][0]
+            long = item['geo']['coordinates'][1]
+            date = item['created_at']
+            text = item['text'].encode('utf-8')
+            output.append((date, text, lat, long))
+        except:
+            error = "no coordinates"
+    return output
 
 
 def link_https_in_text(text):
     parsed_text = re.sub('http://[^ ,]*',
-    		lambda t: "<a href='%s'>%s</a>" %
-			(t.group(0), t.group(0)), text)
+                         lambda t: "<a href='%s'>%s</a>" %
+                         (t.group(0), t.group(0)), text)
     return parsed_text
 
 
@@ -104,7 +104,8 @@ def write_html(tweets, output_file):
     html_output = ""
     for item in tweets:
         d = datetime.strptime(item['created_at'],
-            '%a %b %d %H:%M:%S +0000 %Y') - timedelta(hours=5)
+                              '%a %b %d %H:%M:%S +0000 %Y')
+        - timedelta(hours=5)
         day_string = d.strftime('%d %b %Y %I:%M %p')
         true_time_object = d + timedelta(hours=5)
         time_element = true_time_object.isoformat("T")
@@ -113,15 +114,15 @@ def write_html(tweets, output_file):
                      % (params['twitter_user_id'], item['id'])
         html_output += '<li id=%s>%s - <a href="%s">'\
                        '<time datetime="%s">%s</time></a></li>\n' \
-                        % (item['id'],
-                           text,
-                           tweet_link,
-                           time_element,
-                           day_string)
+            % (item['id'],
+                text,
+                tweet_link,
+                time_element,
+                day_string)
     with open(output_file, "w") as f:
         f.write('<!DOCTYPE html>\n'
-        		'<title>Twitter Archive Output</title>\n'
-        		'<ul>\n')
+                '<title>Twitter Archive Output</title>\n'
+                '<ul>\n')
         f.write(html_output.encode('utf-8'))
         f.write('</ul>')
 
@@ -158,9 +159,9 @@ def write_text(tweets, output_file):
 
 
 def write_csv(d, csv_file):
-	with open(csv_file, 'w') as f:
-		writer = csv.writer(f)
-		writer.writerows(d)
+    with open(csv_file, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(d)
 
 
 def write_json(json_data, output_file):
@@ -168,17 +169,16 @@ def write_json(json_data, output_file):
         f.write(json.dumps(json_data, indent=4))
 
 
-
 def main():
-	d = load_data(params['data_files'])
-	#get_bigrams(d)
-	write_csv(get_bffs(d), params['bff_output'], )
-	write_csv(get_geo(d), params['geo_output'])
-	write_html(d, params['html_output'])
-	write_text(d, params['text_output'])
-	write_json(d, params['json_output'])
-	write_sqlite3(d, params['sqlite3_output'])
+    d = load_data(params['data_files'])
+    #get_bigrams(d)
+    write_csv(get_bffs(d), params['bff_output'], )
+    write_csv(get_geo(d), params['geo_output'])
+    write_html(d, params['html_output'])
+    write_text(d, params['text_output'])
+    write_json(d, params['json_output'])
+    write_sqlite3(d, params['sqlite3_output'])
 
 
-if __name__=="__main__":
-	main()
+if __name__ == "__main__":
+    main()
